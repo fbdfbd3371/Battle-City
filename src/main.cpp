6a615a8823c963a470a0a46cf5fb12884e49720f
Nikita <fbdfbd33714@gmail.com>
2023-08-14 18:24:01 +0300

    OpenGL and glfw intialization

--- src/main.cpp
+++ src/main.cpp
@@ -1,46 +1,86 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
+
+int g_windowSizeX = 640;
+int g_windowSizeY = 480;
+
+// Колбэк изменения размера окна
+void glWindowSizeCallback(GLFWwindow* pWindow, int width, int height)
+{
+    g_windowSizeX = width;
+    g_windowSizeY = height;
+    glViewport(0, 0, g_windowSizeX, g_windowSizeY); // область, в которой мы хотим рисовать.  0, 0 - левый нижний угол
+}
+
+// Колбэк нажатия клавиш
+void glfwKeyCallback(GLFWwindow* pWindow, int key, int scancode, int action, int mode)
+{
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+    {
+        glfwSetWindowShouldClose(pWindow, GL_TRUE); // закрываем окно
+
+    }
+}
 int main(void)
 {
-    GLFWwindow* window;
+    GLFWwindow* pWindow;
 
     /* Initialize the library */
     if (!glfwInit())
+    {
+        std::cout << "glfwInit failed!" << std::endl;
         return -1;
+    }
+
+    // здесь указывается минимально возможная рабочая версия OpenGL
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
-    if (!window)
+    pWindow = glfwCreateWindow(g_windowSizeX, g_windowSizeY, "Battle City", nullptr, nullptr);
+    if (!pWindow)
     {
+        std::cout << "glfwCreateWindow failed!" << std::endl;
         glfwTerminate();
         return -1;
     }
 
+    glfwSetWindowSizeCallback(pWindow, glWindowSizeCallback);
+    glfwSetKeyCallback(pWindow, glfwKeyCallback);
+
     /* Make the window's context current */
-    glfwMakeContextCurrent(window);
+    glfwMakeContextCurrent(pWindow); // для текущего окна
 
 	if(!gladLoadGL())
 	{
 		std::cout<<"Can't load GLAD!\n";
 		return -1;
 	}
+    std::cout << "Renderer: " << glGetString(GL_RENDERER) << std::endl;
+    std::cout << "OpenGL version: " << glGetString(GL_VERSION) << std::endl;
 
 	std::cout<< "OpenGL "<< GLVersion.major << "."<<GLVersion.minor<<std::endl;
+    
 	
 	glClearColor(0, 1, 0, 1);
 
     /* Loop until the user closes the window */
-    while (!glfwWindowShouldClose(window))
+    // Это цикл отрисовки. Он будет продолжаться, пока окно не будет закрыто
+    while (!glfwWindowShouldClose(pWindow))
     {
         /* Render here */
+        // Очищает буффер цвета
         glClear(GL_COLOR_BUFFER_BIT);
 
         /* Swap front and back buffers */
-        glfwSwapBuffers(window);
+        // Меняет местами задний и передний буффер цвета
+        // Видеокарта рисует только задний буффер. А монитор отображает только передний буффер.
+        glfwSwapBuffers(pWindow);
 
         /* Poll for and process events */
-        glfwPollEvents();
+        glfwPollEvents(); // Нажатие клавиш, изменение курсора и т.п.
     }
 
     glfwTerminate();
