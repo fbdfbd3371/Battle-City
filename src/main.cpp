5d317cde591aa1d3801657e75c55a6c62c20688c
Nikita <fbdfbd33714@gmail.com>
2023-08-15 20:41:33 +0300

    Added ResourceManafger

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,8 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 
+#include "Renderer/ShaderProgram.h"
+#include "Resources/ResourceManager.h"
 GLfloat point[] =
 {
     0.0f,   0.5f,   0.0f,
@@ -54,8 +56,9 @@ void glfwKeyCallback(GLFWwindow* pWindow, int key, int scancode, int action, int
 
     }
 }
-int main(void)
+int main(int argc, char** argv)
 {
+
     GLFWwindow* pWindow;
 
     /* Initialize the library */
@@ -98,65 +101,62 @@ int main(void)
 	
 	glClearColor(0, 1, 0, 1);
 
+    
 
-    GLuint vs = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vs, 1, &vertex_shader, nullptr);
-    glCompileShader(vs);
 
-    GLuint fs = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fs, 1, &fragment_shader, nullptr);
-    glCompileShader(fs);
-
-    GLuint shader_programm = glCreateProgram();
-    glAttachShader(shader_programm, vs);
-    glAttachShader(shader_programm, fs);
-
-    glLinkProgram(shader_programm);
-
-    glDeleteShader(vs);
-    glDeleteShader(fs);
-
-    GLuint points_vbo = 0;
-    glGenBuffers(1, &points_vbo);
-    glBindBuffer(GL_ARRAY_BUFFER, points_vbo);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(point), point, GL_STATIC_DRAW);
-
-    GLuint colors_vbo = 0;
-    glGenBuffers(1, &colors_vbo);
-    glBindBuffer(GL_ARRAY_BUFFER, colors_vbo);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(colors), colors, GL_STATIC_DRAW);
-
-    GLuint vao = 0; // vertex array object
-    glGenVertexArrays(1, &vao);
-    glBindVertexArray(vao);
-
-    glEnableVertexAttribArray(0);
-    glBindBuffer(GL_ARRAY_BUFFER, points_vbo);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
-
-    glEnableVertexAttribArray(1);
-    glBindBuffer(GL_ARRAY_BUFFER, colors_vbo);
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
-
-    /* Loop until the user closes the window */
-    // Это цикл отрисовки. Он будет продолжаться, пока окно не будет закрыто
-    while (!glfwWindowShouldClose(pWindow))
     {
-        /* Render here */
-        // Очищает буффер цвета
-        glClear(GL_COLOR_BUFFER_BIT);
+        ResourceManager resourceManager(argv[0]);
+        auto pDefaultShaderProgram = resourceManager.loadShaders("DefaultShader", "res/shaders/vertex.txt", "res/shaders/fragment.txt");
+        if (!pDefaultShaderProgram)
+        {
+            std::cerr << "Can't create shader program: " << "DefaultShader" << std::endl;
+            return -1;
+        }
 
-        glUseProgram(shader_programm);
+        
+
+        GLuint points_vbo = 0;
+        glGenBuffers(1, &points_vbo);
+        glBindBuffer(GL_ARRAY_BUFFER, points_vbo);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(point), point, GL_STATIC_DRAW);
+
+        GLuint colors_vbo = 0;
+        glGenBuffers(1, &colors_vbo);
+        glBindBuffer(GL_ARRAY_BUFFER, colors_vbo);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(colors), colors, GL_STATIC_DRAW);
+
+        GLuint vao = 0; // vertex array object
+        glGenVertexArrays(1, &vao);
         glBindVertexArray(vao);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
 
-        /* Swap front and back buffers */
-        // Меняет местами задний и передний буффер цвета
-        // Видеокарта рисует только задний буффер. А монитор отображает только передний буффер.
-        glfwSwapBuffers(pWindow);
+        glEnableVertexAttribArray(0);
+        glBindBuffer(GL_ARRAY_BUFFER, points_vbo);
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
 
-        /* Poll for and process events */
-        glfwPollEvents(); // Нажатие клавиш, изменение курсора и т.п.
+        glEnableVertexAttribArray(1);
+        glBindBuffer(GL_ARRAY_BUFFER, colors_vbo);
+        glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
+
+        /* Loop until the user closes the window */
+        // Это цикл отрисовки. Он будет продолжаться, пока окно не будет закрыто
+        while (!glfwWindowShouldClose(pWindow))
+        {
+            /* Render here */
+            // Очищает буффер цвета
+            glClear(GL_COLOR_BUFFER_BIT);
+
+            pDefaultShaderProgram->use();
+            glBindVertexArray(vao);
+            glDrawArrays(GL_TRIANGLES, 0, 3);
+
+            /* Swap front and back buffers */
+            // Меняет местами задний и передний буффер цвета
+            // Видеокарта рисует только задний буффер. А монитор отображает только передний буффер.
+            glfwSwapBuffers(pWindow);
+
+            /* Poll for and process events */
+            glfwPollEvents(); // Нажатие клавиш, изменение курсора и т.п.
+        }
     }
 
     glfwTerminate();
