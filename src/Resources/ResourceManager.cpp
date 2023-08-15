b97ef773484fa85a50b8b2e86eed2e5f9daa46d4
Nikita <fbdfbd33714@gmail.com>
2023-08-15 21:06:05 +0300

    add stm_image.h and load a pixels

--- src/Resources/ResourceManager.cpp
+++ src/Resources/ResourceManager.cpp
@@ -5,6 +5,10 @@
 #include <fstream>
 #include <iostream>
 
+#define STB_IMAGE_IMPLEMENTATION
+#define STBI_ONLY_PNG
+#include "stb_image.h"
+
 ResourceManager::ResourceManager(const std::string& excutablePath)
 {
 	size_t found = excutablePath.find_last_of("/\\");
@@ -68,4 +72,21 @@ std::shared_ptr<Renderer::ShaderProgram> ResourceManager::getShaderProgram(const
 	}
 	std::cerr << "Can't find the shader program: " << shaderName << std::endl;
 	return nullptr;
+}
+
+void ResourceManager::loadTexture(const std::string& textureName, const std::string& texturePath)
+{
+	int chanels = 0;
+	int width = 0;
+	int height = 0;
+	stbi_set_flip_vertically_on_load(true);
+	unsigned char* pixels = stbi_load(std::string(m_path + "/" + texturePath).c_str(), &width, &height, &chanels, 0);
+
+	if (!pixels)
+	{
+		std::cerr << "Can't load image: " << texturePath << std::endl;
+		return;
+	}
+
+	stbi_image_free(pixels);
 }
